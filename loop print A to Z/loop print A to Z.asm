892784cd6644659290b0e92eed54448d6cdfef41
Shuvanon Razik <razik666@gmail.com>
2016-12-30 17:48:50 +0600

    loop print A to Z

---
+++ loop print A to Z/loop print A to Z.asm
@@ -0,0 +1,17 @@
+.code
+main proc
+        mov cx,26
+        mov ah,2
+        mov dl, 'A'
+        
+        top:
+        int 21h
+        inc dl
+        
+        loop top
+        
+        mov ah, 4ch
+        int 21h
+        
+        main endp
+end main
